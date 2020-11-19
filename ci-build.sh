bb86b407e8bc9a73f03de2c93c982922851aad2d
P1ratRuleZZZ <p1ratrulezzz@users.noreply.github.com>
2020-11-19 14:36:26 +0300

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -12,21 +12,15 @@ touch "${KEY_PATH}"
 chmod 0600 "${KEY_PATH}"
 echo "${SSH_PRIVATEKEY}" > "${KEY_PATH}"
 ssh-add -t 3600 "${KEY_PATH}"
-sudo apt install -y python3-venv git
 echo "Checkout master"
 git fetch origin
 git checkout -b master origin/master
 git pull origin master
-echo "Deleting words dir"
-git rm -r words
-mkdir words
-echo "Renegerating..."
-chmod +x ./regenerate_html.sh
-./regenerate_html.sh
+echo "Updating the og id"
+php replace_og_id.php
 echo "Add to git"
-git add words
-git add index.html sitemap.xml
-git commit -m"[skip-ci] Words CI update"
+git add index.html
+git commit -m"[skip-ci] OG ID CI update"
 git push -f origin master
 rm -f "${KEY_PATH_PUB}"
 rm -f "${KEY_PATH_PUB}"
